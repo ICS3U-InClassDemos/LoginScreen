f1fa8310d4f2c2c19b8b6528b172f18d8ecc3bb9
Tony Theodoropoulos <theodot@outlook.com>
2021-02-09 14:05:44 -0500

    Add project files.

---
+++ LoginScreen/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace LoginScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userLabel = new System.Windows.Forms.Label();
+            this.pinLabel = new System.Windows.Forms.Label();
+            this.usernameInput = new System.Windows.Forms.TextBox();
+            this.pinInput = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.adminButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userLabel.Location = new System.Drawing.Point(88, 102);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(105, 24);
+            this.userLabel.TabIndex = 0;
+            this.userLabel.Text = "UserName:";
+            // 
+            // pinLabel
+            // 
+            this.pinLabel.AutoSize = true;
+            this.pinLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pinLabel.Location = new System.Drawing.Point(148, 160);
+            this.pinLabel.Name = "pinLabel";
+            this.pinLabel.Size = new System.Drawing.Size(45, 24);
+            this.pinLabel.TabIndex = 1;
+            this.pinLabel.Text = "PIN:";
+            // 
+            // usernameInput
+            // 
+            this.usernameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameInput.Location = new System.Drawing.Point(199, 99);
+            this.usernameInput.Name = "usernameInput";
+            this.usernameInput.Size = new System.Drawing.Size(100, 29);
+            this.usernameInput.TabIndex = 2;
+            // 
+            // pinInput
+            // 
+            this.pinInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pinInput.Location = new System.Drawing.Point(199, 157);
+            this.pinInput.Name = "pinInput";
+            this.pinInput.Size = new System.Drawing.Size(100, 29);
+            this.pinInput.TabIndex = 3;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(199, 217);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(100, 37);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(88, 330);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(315, 24);
+            this.outputLabel.TabIndex = 5;
+            this.outputLabel.Text = "Please enter your username and pin";
+            // 
+            // adminButton
+            // 
+            this.adminButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adminButton.Location = new System.Drawing.Point(199, 273);
+            this.adminButton.Name = "adminButton";
+            this.adminButton.Size = new System.Drawing.Size(100, 37);
+            this.adminButton.TabIndex = 6;
+            this.adminButton.Text = "Admin";
+            this.adminButton.UseVisualStyleBackColor = true;
+            this.adminButton.Click += new System.EventHandler(this.adminButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 450);
+            this.Controls.Add(this.adminButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.pinInput);
+            this.Controls.Add(this.usernameInput);
+            this.Controls.Add(this.pinLabel);
+            this.Controls.Add(this.userLabel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label pinLabel;
+        private System.Windows.Forms.TextBox usernameInput;
+        private System.Windows.Forms.TextBox pinInput;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button adminButton;
+    }
+}
+
---
+++ LoginScreen/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginScreen
+{
+    public partial class Form1 : Form
+    {
+        string userName;
+        int pin;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                userName = usernameInput.Text;
+                pin = Convert.ToInt32(pinInput.Text);
+
+                outputLabel.Text = $"{userName} {pin}";
+            }
+            catch
+            {
+                outputLabel.Text = "Please enter numbers only for PIN";
+            }
+        }
+
+        private void adminButton_Click(object sender, EventArgs e)
+        {
+            outputLabel.Text = $"{pin} {userName} ";
+        }
+    }
+}
